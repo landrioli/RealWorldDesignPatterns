75175644beaed6348dd06fc1ad641aee5fc55e56
Leandro <leandro.andrioli@outlook.com>
2020-01-21 13:14:07 +0000

    Singleton Pattern

---
+++ RealWorldDesignPatterns/Creational/SingletonPattern/Monostate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldDesignPatterns.Creational.SingletonPattern
+{
+    class Monostate
+    {
+        //Non-Static class but state is static
+        public class ChiefExecutiveOfficer
+        {
+            private static string name;
+            private static int age;
+
+            public string Name
+            {
+                get => name;
+                set => name = value;
+            }
+
+            public int Age
+            {
+                get => age;
+                set => age = value;
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+            }
+        }
+    }
+}
---
+++ RealWorldDesignPatterns/Creational/SingletonPattern/Singleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldDesignPatterns.Creational.SingletonPattern
+{
+    public class Singleton
+    {
+        public interface IDatabase
+        {
+            int GetPopulation(string name);
+        }
+
+        public class DummyDatabase : IDatabase
+        {
+            public int GetPopulation(string name)
+            {
+                return 1;
+            }
+        }
+
+        public class SingletonDatabase : IDatabase
+        {
+            public static int Count => instanceCount;
+            private static int instanceCount;
+
+            private SingletonDatabase()
+            {
+               Console.WriteLine("Initializing database");
+            }
+
+            public int GetPopulation(string name)
+            {
+                return 3;
+            }
+
+            // laziness + thread safety
+            private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() =>
+            {
+                instanceCount++;
+                return new SingletonDatabase();
+            });
+
+            public static IDatabase Instance => instance.Value;
+        }
+    }
+}
