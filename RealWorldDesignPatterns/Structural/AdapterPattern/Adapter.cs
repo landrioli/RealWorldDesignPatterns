9c7c31fe550cc6d602319b0c68023f2e0578181f
Leandro <leandro.andrioli@outlook.com>
2020-01-28 08:31:10 +0000

    Bridge and Composite Pattern

---
+++ RealWorldDesignPatterns/Structural/AdapterPattern/Adapter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldDesignPatterns.Structural.AdapterPattern
+{
+    public class AdapterPack
+    {
+        public class Target
+
+        {
+            public virtual void Request()
+            {
+                Console.WriteLine("Called Target Request()");
+            }
+        }
+
+        public class Adapter : Target
+        {
+            private readonly Adaptee _adaptee = new Adaptee();
+
+            public override void Request()
+            {
+                // Possibly do some other work... and then call SpecificRequest
+                _adaptee.SpecificRequest();
+            }
+        }
+
+        public class Adaptee
+        {
+            public void SpecificRequest()
+            {
+                Console.WriteLine("Called SpecificRequest()");
+            }
+        }
+
+        public void Run()
+        {
+            Target target = new Adapter();
+            target.Request();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RealWorldDesignPatterns/Structural/BridgePattern/Bridge.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealWorldDesignPatterns.Structural.BridgePattern
+{
+    class Bridge
+    {
+        public interface IRenderer
+        {
+            void RenderCircle(float radius);
+        }
+
+        public class VectorRenderer : IRenderer
+        {
+            public void RenderCircle(float radius)
+            {
+                Console.WriteLine($"Drawing a circle of radius {radius}");
+            }
+        }
+
+        public class RasterRenderer : IRenderer
+        {
+            public void RenderCircle(float radius)
+            {
+                Console.WriteLine($"Drawing pixels for circle of radius {radius}");
+            }
+        }
+
+        public abstract class Shape
+        {
+            protected IRenderer renderer;
+
+            // a bridge between the shape that's being drawn an
+            // the component which actually draws it
+            public Shape(IRenderer renderer)
+            {
+                this.renderer = renderer;
+            }
+
+            public abstract void Draw();
+            public abstract void Resize(float factor);
+        }
+
+        public class Circle : Shape
+        {
+            private float radius;
+
+            public Circle(IRenderer renderer, float radius) : base(renderer)
+            {
+                this.radius = radius;
+            }
+
+            public override void Draw()
+            {
+                renderer.RenderCircle(radius);
+            }
+
+            public override void Resize(float factor)
+            {
+                radius *= factor;
+            }
+        }
+    }
+}
